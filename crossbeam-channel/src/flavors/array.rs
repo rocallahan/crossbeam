6b70eb44c12a0db088751571f411362e7c411dad
caelunshun <caelunshun@gmail.com>
2020-06-25 22:04:33 -0600

    Use Box<[T]> instead of Vec<T> to initialize and drop ArrayQueue
    Previously, the queue buffer was initialized using Vec::with_capacity.
    Note the with_capacity does not guarantee that precise capacity;
    the actual layout of the allocated memory could have a greater size.
    
    The drop code assumed the buffer has a size
    equal to the queue's capacity. This is undefined
    behavior when the buffer actually has a greater size.
    
    Using Box<[T]> instead guarantees an exact capacity, resolving the UB.

--- crossbeam-channel/src/flavors/array.rs
+++ crossbeam-channel/src/flavors/array.rs
@@ -117,7 +117,7 @@ impl<T> Channel<T> {
         // Allocate a buffer of `cap` slots initialized
         // with stamps.
         let buffer = {
-            let mut v: Vec<Slot<T>> = (0..cap)
+            let mut boxed: Box<[Slot<T>]> = (0..cap)
                 .map(|i| {
                     // Set the stamp to `{ lap: 0, mark: 0, index: i }`.
                     Slot {
@@ -126,8 +126,8 @@ impl<T> Channel<T> {
                     }
                 })
                 .collect();
-            let ptr = v.as_mut_ptr();
-            mem::forget(v);
+            let ptr = boxed.as_mut_ptr();
+            mem::forget(boxed);
             ptr
         };
 
@@ -555,7 +555,11 @@ impl<T> Drop for Channel<T> {
 
         // Finally, deallocate the buffer, but don't run any destructors.
         unsafe {
-            Vec::from_raw_parts(self.buffer, 0, self.cap);
+            // Create a slice from the buffer to make
+            // a fat pointer. Then, use Box::from_raw
+            // to deallocate it.
+            let ptr = std::slice::from_raw_parts_mut(self.buffer, self.cap) as *mut [Slot<T>];
+            Box::from_raw(ptr);
         }
     }
 }
